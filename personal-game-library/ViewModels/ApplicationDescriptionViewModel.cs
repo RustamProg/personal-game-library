fdf9ec4c18c78c1e36dfdf3dcbfcb3339f4241f4
RustamProg <57099791+RustamProg@users.noreply.github.com>
2021-02-17 23:18:51 +0500

    Added description window view and its viewmodel Need to implement functionality

---
+++ personal-game-library/ViewModels/ApplicationDescriptionViewModel.cs
@@ -0,0 +1,38 @@
+﻿using personal_game_library.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace personal_game_library.ViewModels
+{
+    class ApplicationDescriptionViewModel : INotifyPropertyChanged
+    {
+
+        private Userapp currentUserApp;
+        public ApplicationDescriptionViewModel()
+        {
+
+        }
+
+        public Userapp CurrentUserApp
+        {
+            get { return currentUserApp; }
+            set
+            {
+                currentUserApp = value;
+                OnPropertyChanged("CurrentUserApp");
+            }
+        }
+
+        // Property Changed Event Handler
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ personal-game-library/Views/ApplicationDescriptionView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using personal_game_library.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace personal_game_library.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для ApplicationDescriptionView.xaml
+    /// </summary>
+    public partial class ApplicationDescriptionView : Window
+    {
+        public ApplicationDescriptionView()
+        {
+            InitializeComponent();
+            this.DataContext = new ApplicationDescriptionViewModel();
+        }
+    }
+}
